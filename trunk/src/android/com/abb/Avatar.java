cc1febaeabf548b9d67e8880e703334a9fb5665a
matt.burkhart <matt.burkhart@676ebac8-b1cf-11dd-9e4a-13528392d412>
2008-11-19 02:43:46 +0000

    Finished up new shooting animations and view controls.

--- trunk/src/android/com/abb/Avatar.java
+++ trunk/src/android/com/abb/Avatar.java
@@ -42,12 +42,12 @@ public class Avatar extends Entity {
       ddx = -kAirAcceleration;
 
     // Update the avatar animation frame according the current entity motion.
-    int sprite_index = 0;
     if (dx < 0)
       sprite_offset_ = 9;
     else if (dx > 0)
       sprite_offset_ = 0;
 
+    int sprite_index = 0;
     if (has_ground_contact) {
       if (Math.abs(dx) > kAnimationStopThreshold) {
         animation_phase_ += kGroundAnimationSpeed * Math.abs(dx);
@@ -64,36 +64,48 @@ public class Avatar extends Entity {
 
     SetSprite(sprite_offset_ + sprite_index);
 
-    // Update the shooting mechanism.
+    // Update the shooting mechanism. The choices for shot direction are
+    // specialized for each animation case: in the air, facing left, right, and
+    // considering the avatar's speed.
     shot_delay_ -= time_step;
     if (shooting_ && shot_delay_ < time_step) {
       shot_delay_ = kShotDelay;
-
       float shot_angle;
+      float shot_distance = kShotDistance;
+      float shot_velocity = kShotVelocity;
+      float x_offset;
+      float y_offset;
       boolean facing_left = (sprite_offset_ == 9);
+
       if (!has_ground_contact) {
         shot_angle = shot_phase_;
         if (facing_left)
           shot_angle = -shot_phase_;
         shot_delay_ -= 2.0f * time_step;
         shot_phase_ += 45.0f * (float)Math.PI / 180.0f;
+        shot_velocity *= 0.6f;
+        x_offset = kShotDistance * (float)Math.cos(shot_angle);
+        y_offset = kShotDistance * (float)Math.sin(shot_angle);
       } else if (facing_left) {
         shot_angle = kShotSpread * (float)Math.sin(shot_phase_) + (float)Math.PI;
         shot_phase_ += 10.0f;
+        x_offset = -kShotOffsetX;
+        y_offset = kShotOffsetY;
+        if (Math.abs(dx) > kAnimationStopThreshold)
+          y_offset += kShotDistance / 2.0f * (float)Math.sin(shot_phase_);
       } else {
         shot_angle = kShotSpread * (float)Math.sin(shot_phase_);
         shot_phase_ += 10.0f;
+        x_offset = kShotOffsetX;
+        y_offset = kShotOffsetY;
+        if (Math.abs(dx) > kAnimationStopThreshold)
+          y_offset += kShotDistance / 2.0f * (float)Math.sin(shot_phase_);
       }
 
-      float cos_shot_angle = (float)Math.cos(shot_angle);
-      float sin_shot_angle = (float)Math.sin(shot_angle);
-      float x_offset = kShotDistance * cos_shot_angle;
-      float y_offset = kShotDistance * sin_shot_angle;
-      float dx_offset = kShotVelocity * cos_shot_angle;
-      float dy_offset = kShotVelocity * sin_shot_angle;
-      Fire fire = game_state_.CreateFireProjectile(
+      float dx_offset = shot_velocity * (float)Math.cos(shot_angle);
+      float dy_offset = shot_velocity * (float)Math.sin(shot_angle);
+      game_state_.CreateFireProjectile(
           x + x_offset, y + y_offset, dx + dx_offset, dy + dy_offset);
-
     }
   }
 
@@ -113,28 +125,30 @@ public class Avatar extends Entity {
     sprite_source.bottom = kSpriteSize * index + kSpriteSize;
   }
 
+  private float animation_phase_ = 0.0f;
   private GameState game_state_;
   private boolean shooting_;
   private float shot_delay_;
   private float shot_phase_;
-  private float animation_phase_ = 0.0f;
   private int sprite_offset_ = 0;
 
-  private static final float kAirAcceleration = 25.0f;
-  private static final float kAirAnimationSpeed = 4.0f;
+  private static final float kAirAcceleration = 40.0f;
+  private static final float kAirAnimationSpeed = 3.0f;
   private static final float kAnimationStopThreshold = 40.0f;
   private static final float kGravity = 200.0f;
-  private static final float kGroundAcceleration = 300.0f;
-  private static final float kGroundAnimationSpeed = 1.0f / 500.0f;
+  private static final float kGroundAcceleration = 500.0f;
+  private static final float kGroundAnimationSpeed = 1.0f / 600.0f;
   private static final float kJumpVelocity = 250.0f;
-  private static final float kRadius = 27.0f;
-  private static final float kShotDelay = 0.2f;  // Seconds between shots.
-  private static final float kShotDistance = 25.0f;
-  private static final float kShotSpread = 15.0f * (float)Math.PI / 180.0f;  // Radians.
-  private static final float kShotVelocity = 60.0f;
-  private static final int kSpriteSize = 64;
   private static final int kKeyLeft = KeyEvent.KEYCODE_A;
   private static final int kKeyRight = KeyEvent.KEYCODE_S;
   private static final int kKeyJump = KeyEvent.KEYCODE_K;
   private static final int kKeyShoot = KeyEvent.KEYCODE_J;
+  private static final float kRadius = 25.0f;
+  private static final float kShotDelay = 0.2f;  // Seconds between shots.
+  private static final float kShotDistance = 25.0f;
+  private static final float kShotOffsetX = 23.0f;
+  private static final float kShotOffsetY = -8.0f;
+  private static final float kShotSpread = 15.0f * (float)Math.PI / 180.0f;
+  private static final float kShotVelocity = 60.0f;
+  private static final int kSpriteSize = 64;
 }
