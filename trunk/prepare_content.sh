9881101d582670d3447c464a1ec063a74962e3b9
matt.burkhart <matt.burkhart@676ebac8-b1cf-11dd-9e4a-13528392d412>
2009-03-17 00:54:41 +0000

    Improved file management.

--- trunk/prepare_content.sh
+++ trunk/prepare_content.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 TARGET="res/raw/content_package.zip"
-FILES=`find -L content_package | grep -v \~ | grep -v \.svn | grep -v \.drop`
+EPOCH="content_package/epoch.txt"
+
+ALL_FILES=`find -L content_package | grep -v \~ | grep -v \.svn | grep -v \.drop`
 DEMO_FILES=`find -L content_package | grep -v \~ | grep -v \.svn | grep -v \.drop | grep -v Classic | grep -v The_Second_Wave`
+
 rm $TARGET
-zip $TARGET $DEMO_FILES
+rm $EPOCH
+
+cat $ALL_FILES | sha1sum > $EPOCH
+zip $TARGET $FILES $EPOCH
